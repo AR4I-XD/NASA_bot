f478da7cd7d442c8c82a1a55aa8f4c4532cbe5f0
AR4I-XD <hudeevnik@gmail.com>
2025-04-27 15:52:29 +0300

    automatic publishing

--- nasa_bot.py
+++ nasa_bot.py
@@ -1,15 +1,19 @@
 import telegram
 import os
 from dotenv import load_dotenv
+import random
+import time
 
 load_dotenv()
 BOT_API_TOKEN = os.getenv("BOT_API_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
+TIME_DELAY = os.getenv("TIME_DELAY")
+DIRECTORY = "images"
 
 bot = telegram.Bot(token=BOT_API_TOKEN)
-print(bot.get_me())
 
-
-updates = bot.get_updates()
-
-bot.send_photo(chat_id=CHAT_ID, photo=open('images/nasa_apod_26.jpg', 'rb'))
+images = os.listdir(DIRECTORY)
+random.shuffle(images)
+for img in images:
+    bot.send_photo(chat_id=CHAT_ID, photo=open(f'images/{img}', 'rb'))
+    time.sleep(int(TIME_DELAY)*3600)
